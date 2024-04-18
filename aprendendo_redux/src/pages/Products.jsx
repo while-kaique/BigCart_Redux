c64c78178b65419753d97847db24fe1e3119323d
while-kaique <123902283+while-kaique@users.noreply.github.com>
2024-04-18 02:07:10 -0300

    Voltar

--- aprendendo_redux/src/pages/Products.jsx
+++ aprendendo_redux/src/pages/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
   const [uploads, setUploads] = useState([])
   useEffect(()=>{
     setTimeout(() => {
-      fetch("http://bigcart-ecommerce-8952.onrender.com/pictures", {
+      fetch("https://bigcart-ecommerce-8952.onrender.com/pictures", {
         method: 'GET'
       })
       .then((res) => {
